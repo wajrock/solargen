1850c99ce1de3decd84a4edb173b84233375bbb7
wajrock <thibaud.wajrock@outlook.com>
2026-06-14 19:12:28 +0200

    backend: update documentation title page

--- solargen-backend/src/main.ts
+++ solargen-backend/src/main.ts
@@ -21,7 +21,9 @@ async function bootstrap() {
         .build();
 
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('docs', app, document);
+    SwaggerModule.setup('docs', app, document, {
+        customSiteTitle: 'SolarGen Backend API | Documentation',
+    });
 
     app.useGlobalPipes(new ValidationPipe());
     await app.listen(process.env.PORT ?? 3000);
