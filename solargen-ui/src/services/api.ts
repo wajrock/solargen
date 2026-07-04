771c1c62c2e210207fb04994ffe9cb8ec0051672
wajrock <thibaud.wajrock@outlook.com>
2026-07-04 23:50:09 +0200

    ui: update api base url

--- solargen-ui/src/services/api.ts
+++ solargen-ui/src/services/api.ts
@@ -1,6 +1,5 @@
 import axios, {type AxiosInstance} from 'axios';
 
 export const api: AxiosInstance = axios.create({
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: 'https://api--solargen-backend--mdh6jkptypjr.code.run',
 });
